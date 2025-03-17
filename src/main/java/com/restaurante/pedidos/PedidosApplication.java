d35fc25dfa79d510b6a3782c1b2c087d1a10a892
Marlon David Peñuela Pardo <pp.marlondavid@hotmail.com>
2025-03-17 18:53:15 -0500

    feature: Design microservice

--- src/main/java/com/restaurante/pedidos/PedidosApplication.java
+++ src/main/java/com/restaurante/pedidos/PedidosApplication.java
@@ -1,4 +1,6 @@
-public class Main {
+package main.java.com.restaurante.pedidos;
+
+public class PedidosApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
